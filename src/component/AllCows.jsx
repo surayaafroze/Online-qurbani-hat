93e6da8a934e5c1c2845b82d1146a90006c01113
surayaafroze63 <surayaafroze63@gmail.com>
2026-05-01 10:20:59 +0600

    Setup folder structure for components

---
+++ src/component/AllCows.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCows = () => {
+  return (
+    <div>
+      all AllCows
+    </div>
+  );
+};
+
+export default AllCows;
---
+++ src/component/ExtraSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExtraSection = () => {
+  return (
+    <div>
+      ExtraSection
+    </div>
+  );
+};
+
+export default ExtraSection;
