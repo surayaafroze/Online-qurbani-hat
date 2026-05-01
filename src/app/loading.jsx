673d880fdabec7a9409e8d9ff641f06fbfb49ad2
surayaafroze63 <surayaafroze63@gmail.com>
2026-05-02 01:42:26 +0600

    Add loading spriner

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PuffLoader } from 'react-spinners';
 
 const loading = () => {
   return (
-    <div>
-      
+    <div className=' min-h-screen flex justify-center items-center'>
+      <PuffLoader />
     </div>
   );
 };
