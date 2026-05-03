1b788044e0c236963942967d68cc23a7ea27e355
surayaafroze63 <surayaafroze63@gmail.com>
2026-05-03 23:01:36 +0600

    solve some issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="">
