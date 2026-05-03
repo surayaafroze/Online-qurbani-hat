60213d880511d94912347ac83f4ada8657e584da
surayaafroze63 <surayaafroze63@gmail.com>
2026-05-03 15:32:12 +0600

    Added google auth

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
   console.log(user)
   const [isOpen, setIsOpen] = useState(false);
   
-
+const handelSignOut=async()=>{
+await authClient.signOut();
+}
   return (
     <div className=" px-2 bg-[#ccffcc] border-b border-[#004d00] py-2">
       <nav className="flex justify-between items-center py-3 max-w-7xl mx-auto w-full px-3">
@@ -34,8 +36,47 @@ const Navbar = () => {
         {/* Desktop Auth */}
         <div className="hidden md:flex gap-4 text-sm">
           {! user && <ul className="flex justify-center items-center gap-3">
-            <li><Link href="/signup">SignUp</Link></li>
             <li><Link href="/signin">SignIn</Link></li>
+            <li><Link href="/signup">Register</Link></li>
+          </ul>}
+          {user && 
+          <div className="flex justify-center items-center gap-3">
+            <Avatar>
+        <Avatar.Image alt={user?.name} src={user?.image} 
+        referrerPolicy="no-referrer"
+        />
+        <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+      </Avatar>
+            {/* <Avatar>
+        <Avatar.Image alt={user?.image} />
+        <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+      </Avatar> */}
+      <Button onClick={handelSignOut} variant="danger">Sign OUt</Button>
+          </div>
+          }
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div className="md:hidden">
+          <button onClick={() => setIsOpen(!isOpen)}>
+           <IoMdMenu></IoMdMenu>
+          </button>
+        </div>
+      </nav>
+
+      {/* Mobile Dropdown */}
+      {isOpen && (
+        <div className="md:hidden flex flex-col gap-4 px-4 pb-4 text-sm">
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/allAnimals">All Animals</NavLink>
+          <NavLink href="/profile">Profile</NavLink>
+
+          <hr />
+
+           <div className="hidden md:flex gap-4 text-sm">
+          {! user && <ul className="flex justify-center items-center gap-3">
+            <li><Link href="/signin">SignIn</Link></li>
+            <li><Link href="/signup">Register</Link></li>
           </ul>}
           {user && 
           <div className="flex justify-center items-center gap-3">
@@ -53,30 +94,6 @@ const Navbar = () => {
           </div>
           }
         </div>
-
-        {/* Mobile Menu Button */}
-        <div className="md:hidden">
-          <button onClick={() => setIsOpen(!isOpen)}>
-           <IoMdMenu></IoMdMenu>
-          </button>
-        </div>
-      </nav>
-
-      {/* Mobile Dropdown */}
-      {isOpen && (
-        <div className="md:hidden flex flex-col gap-4 px-4 pb-4 text-sm">
-          <NavLink href="/">Home</NavLink>
-          <NavLink href="/allPhotos">All Animals</NavLink>
-          <NavLink href="/profile">Profile</NavLink>
-
-          <hr />
-
-         <div>
-          {<ul>
-            <li><Link href="/signup">SignUp</Link></li>
-            <li><Link href="/signin">SignIn</Link></li>
-          </ul>}
-         </div>
         </div>
       )}
      
